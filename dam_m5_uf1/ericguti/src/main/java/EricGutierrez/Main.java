992d952d9cbe78626b1beb68b03a2e98c89ad716
Eric Gutierrez <egutierre1@inscaparrella.cat>
2023-12-12 19:59:21 +0100

    versio inicial

---
+++ dam_m5_uf1/ericguti/src/main/java/EricGutierrez/Main.java
@@ -0,0 +1,14 @@
+package dam_m5_uf1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola soc el Eric");
+        System.out.println("Hola soc el Eric");
+        System.out.println("Hola soc el Eric");
+        System.out.println("Hola soc el Eric");
+        System.out.println("Hola soc el Eric");
+
+        
+
+    }
+}
